b363fa08d531338bc467108aaa1a49b7f6117a1c
anac157 <danacarolina620@gmail.com>
2025-10-14 19:34:32 -0300

    estrurura_de_decisao_ana

---
+++ estrutura_de_decisao_1.py
@@ -0,0 +1,8 @@
+numero_1=float(input("primeiro número: "))
+numero_2=float(input("segundo número: "))
+if numero_1>numero_2:
+    print(f"(numero_1)é o maior")
+elif numero_1<numero_2:
+    print(f"(numero_2)é o maior")
+else:
+    print("os dois numeros sao iguais.")
