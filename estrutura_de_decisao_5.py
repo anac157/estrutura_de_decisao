67e176b4adcf0b5d80ec3d35a277cd80d441f74a
anac157 <danacarolina620@gmail.com>
2025-10-14 21:35:34 -0300

    estrutura_de_decisao_ana5

---
+++ estrutura_de_decisao_5.py
@@ -0,0 +1,11 @@
+media=float(input("informe a média:"))
+if media>=7:
+  #se
+  print("aprovado")
+  print("parabéns")
+elif media>5:
+  print("em exame")
+  #senao
+else:
+  print("reprovado")
+  print("continue estudando.")
